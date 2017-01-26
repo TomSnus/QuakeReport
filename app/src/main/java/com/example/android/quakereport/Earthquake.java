e8bef321d149a7880fc0380ecc4e08f2c14d2e88
TomSnus <senteswins@gmail.com>
2017-01-26 12:36:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,49 @@
+package com.example.android.quakereport;
+
+/**
+ * Created by Faßreiter on 26.01.2017.
+ */
+
+public class Earthquake {
+
+    private double mMagnitude;
+    private String mLocation;
+    private String mDate;
+
+
+    private String mUrl;
+
+
+    private long mTimeInMilliseconds;
+
+    public Earthquake(double mMagnitude, String mLocation, long timeInMilliseconds, String url) {
+        this.mMagnitude = mMagnitude;
+        this.mLocation = mLocation;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+
+
+
+
+    public double getmMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+    public long getmTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+
+}
